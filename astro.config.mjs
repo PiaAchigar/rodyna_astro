a9ae139474702b18340bcba64c1729060103a1d1
María Pía Achigar <achigarpia@gmail.com>
2025-06-05 16:58:33 -0300

    astro proyect

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://piaachigar.github.io',
+    // site: 'https://www.rodynafarmacias.com.ar',
+    base: '/rodyna_astro/',
+});
